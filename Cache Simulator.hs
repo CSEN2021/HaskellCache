614803e17c6e656965387f8acb92454a73aed646
Mohamed Gallab <moknaruto@gmail.com>
2021-06-20 19:32:54 +0200

    Almost done with all the general part

--- Cache Simulator.hs
+++ Cache Simulator.hs
@@ -4,7 +4,6 @@ data Tag = T Int deriving (Show, Eq)
 data Data a = D a deriving (Show, Eq)
 
 convertBinToDec :: Integral a => a -> a
-
 convertBinToDecHelper 0 _ = 0
 convertBinToDecHelper bin p = ((mod bin 10)* (2 ^ p)) + convertBinToDecHelper (div bin 10) (p + 1)
 convertBinToDec bin = convertBinToDecHelper bin 0
@@ -13,18 +12,16 @@ replaceIthItem :: (Eq a, Num a) => t -> [t] -> a -> [t]
 replaceIthItem a (x:xs) 0 = (a:xs)
 replaceIthItem a (x:xs) i = x:(replaceIthItem a xs (i-1))
 
-{-splitEvery :: Int -> [a] -> [[a]]
-splitEvery n l = [splitEveryHelper n n l]
-splitEveryHelper _ _ [] = []
-splitEveryHelper n 0 (x:xs) = (x:(splitEveryHelper n n xs))
-splitEveryHelper n c (x:xs) = [x] ++ splitEveryHelper n (c-1) xs-}
-
-splitEvery n l = if 
-splitEvery n (x:xs) = (a:splitEvery n xs) where (a,b) = splitAt n (x:xs)
+splitEvery :: Int -> [a] -> [[a]]
+splitEvery n (x:xs) 
+    | (length (x:xs)) <= n = [(x:xs)]
+    | otherwise = (a: (splitEvery n b)) where (a,b) = splitAt n (x:xs)
 
 logBase2 :: Floating a => a -> a
 logBase2 num = logBase 2 num
 
+getNumBits :: (Integral a, RealFloat a1) => a1 -> [Char] -> [c] -> a
+getNumBits numOfSets cacheType cache = ceiling (logBase2 numOfSets)
 
 --Component 1
 
